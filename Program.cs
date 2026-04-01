9ca260da12ba9fe40c90f91d4155d9ff473ecef7
Abbos <abbos.abduqodirov.070405@gmail.com>
2026-04-01 11:39:18 +0500

    feat: dobavlen noviy function dlya vicislenie chisel

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ class Calculator
     public int Subtract(int a, int b) => a - b;
 
     public int Multiply(int a, int b) => a * b;
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
 }
 
 
