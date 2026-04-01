93c1033ab5fac431f04e45f302126d26f51ca45c
Abbos <abbos.abduqodirov.070405@gmail.com>
2026-04-01 11:48:02 +0500

    https://github.com/abbos-ab/AppForLearningGitHub/pull/new/fun-sum

--- Program.cs
+++ Program.cs
@@ -1,21 +1,4 @@
-﻿Calculator calculator = new Calculator();
-var response = calculator.Add(5, 10, 7);
-Console.WriteLine(response);
-
-class Calculator
-{
-    public int Add(int a, int b, int c) => a + b + c;
-    public int Subtract(int a, int b) => a - b;
-
-    public int Multiply(int a, int b) => a * b;
-
-    public int Divide(int a, int b)
-    {
-        return a / b;
-    }
-}
-
-class Info
+﻿class Info
 {
     public void SayHello()
     {
