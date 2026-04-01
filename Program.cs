b8990a313eded5cf550d853285f22f084846b512
Abbos <abbos.abduqodirov.070405@gmail.com>
2026-04-01 11:34:56 +0500

    feat: dobavlen noviy fishka dlya function Add

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿Calculator calculator = new Calculator();
-var response = calculator.Add(5, 10);
+var response = calculator.Add(5, 10, 7);
 Console.WriteLine(response);
 
 class Calculator
 {
-    public int Add(int a, int b) => a + b;
+    public int Add(int a, int b, int c) => a + b + c;
     public int Subtract(int a, int b) => a - b;
 }
 
